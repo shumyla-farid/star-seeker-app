e432d8e48857dcfffe574e55e275eecf6d60067a
Shumyla Farid <64152859+shumyla-farid@users.noreply.github.com>
2025-12-14 21:07:59 +0000

    add types for net info mock

---
+++ src/types/netinfo-jest-mock.d.ts
@@ -0,0 +1,9 @@
+declare module "@react-native-community/netinfo/jest/netinfo-mock" {
+  const mock: any;
+  export default mock;
+}
+
+declare module "@react-native-community/netinfo/jest/netinfo-mock.js" {
+  const mock: any;
+  export default mock;
+}
